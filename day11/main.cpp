9d6e1a315808e54ae51bcb536dbc071e32101f5f
Ben Smith <smithbenjamin@ufl.edu>
2023-12-18 16:54:15 -0500

    day11 part 2

--- day11/main.cpp
+++ day11/main.cpp
@@ -5,24 +5,25 @@
 
 int main() {
 
-    std::ifstream input("test-input.txt");
+    std::ifstream input("input.txt");
     std::string row;
     std::vector<std::string> observatory;
     std::vector<std::vector<int>> distanceGraph;
 
-    int expandSize = 2;
+    int expandSize = 1000000;
     // expand rows
     while (input >> row) {
         std::vector<int> distances;
         distances.reserve(row.size());
         for (int i = 0; i < row.size(); i++) {
-            distances.push_back(1);
+            distances.push_back(0);
         }
-        distanceGraph.push_back(distances);
 
         if (std::all_of(row.begin(), row.end(), [](char i){return i == '.';})) {
-            observatory.push_back(row);
             std::fill(distances.begin(), distances.end(), expandSize);
+            distanceGraph.push_back(distances);
+        } else {
+            std::fill(distances.begin(), distances.end(), 1);
             distanceGraph.push_back(distances);
         }
         observatory.push_back(row);
@@ -38,18 +39,15 @@ int main() {
             }
         }
         if (expand) {
-            for (auto& line: observatory) {
-                line.insert(i, 1, '.');
-            }
             for (auto& distances: distanceGraph) {
-                distances.insert(distances.begin() + i, expandSize);
+                distances[i] = expandSize;
             }
             i++;
         }
     }
 
     std::vector<std::pair<int, int>> galaxies;
-    int sum = 0;
+    unsigned long long sum = 0;
 
     for (int i = 0; i < observatory.size(); i++) {
         for (int j = 0; j < observatory[i].size(); j++) {
@@ -65,14 +63,14 @@ int main() {
             int start = std::min(std::get<1>(galaxy1), std::get<1>(galaxy2));
             int end = std::max(std::get<1>(galaxy1), std::get<1>(galaxy2));
             for (int x = start; x < end; x++) {
-                xDist += distanceGraph[0][x];
+                xDist += distanceGraph[std::get<0>(galaxy1)][x];
             }
 
             int yDist = 0;
             start = std::min(std::get<0>(galaxy1), std::get<0>(galaxy2));
             end = std::max(std::get<0>(galaxy1), std::get<0>(galaxy2));
             for (int x = start; x < end; x++) {
-                yDist += distanceGraph[x][0];
+                yDist += distanceGraph[x][std::get<1>(galaxy1)];
             }
 
             sum += xDist + yDist;
