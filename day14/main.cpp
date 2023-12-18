9e4dd3807d36cc71ba9fe806459664f8d8404bcf
Ben Smith <smithbenjamin@ufl.edu>
2023-12-18 18:53:19 -0500

    day14 part2 almost done

--- day14/main.cpp
+++ day14/main.cpp
@@ -2,18 +2,8 @@
 #include <fstream>
 #include <vector>
 
-int main() {
-
-    std::ifstream input("input.txt");
-    std::string line;
-
-    std::vector<std::string> rocks;
-
-    while (input >> line) {
-        rocks.push_back(line);
-    }
-
-    // roll forward
+void spinCycle(std::vector<std::string>& rocks) {
+    // roll north
     for (int row = 0; row < rocks.size(); row++) {
         for (int rock = 0; rock < rocks[row].size(); rock++) {
             int i = 1;
@@ -26,6 +16,85 @@ int main() {
             }
         }
     }
+    // roll west
+    for (int col = 0; col < rocks[0].size(); col++) {
+        for (auto& row : rocks) {
+            int i = 1;
+            if (row[col] == 'O') {
+                while (col - i >= 0 && row[col - i] == '.') {
+                    row[col - i] = 'O';
+                    row[col - i + 1] = '.';
+                    i++;
+                }
+            }
+        }
+    }
+
+    // roll south
+    for (int row = (int)rocks.size() - 1; row >= 0; row--) {
+        for (int rock = 0; rock < rocks[row].size(); rock++) {
+            int i = 1;
+            if (rocks[row][rock] == 'O') {
+                while (row + i < rocks.size() && rocks[row + i][rock] == '.') {
+                    rocks[row + i][rock] = 'O';
+                    rocks[row + i - 1][rock] = '.';
+                    i++;
+                }
+            }
+        }
+    }
+
+    // roll east
+    for (int col = (int)rocks[0].size() - 1; col >= 0; col--) {
+        for (auto& row: rocks) {
+            int i = 1;
+            if (row[col] == 'O') {
+                while (col + i < rocks[0].size() && row[col + i] == '.') {
+                    row[col + i] = 'O';
+                    row[col + i - 1] = '.';
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+
+    std::ifstream input("test-input.txt");
+    std::string line;
+
+    std::vector<std::string> rocks;
+
+    while (input >> line) {
+        rocks.push_back(line);
+    }
+
+    std::vector<std::vector<std::string>> prev;
+    int cycleStart;
+    int cycleEnd;
+    for (unsigned long long i = 0; i < 1000000000; i++) {
+        spinCycle(rocks);
+        prev.push_back(rocks);
+        bool found = true;
+        int j;
+        for (j = 0; j < prev.size(); i++) {
+            for (int k = 0; k < rocks.size(); k++) {
+                if (prev[j][k] != rocks[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (!found) { break; }
+        }
+
+        if (found) {
+            cycleStart = j;
+            cycleEnd = (int)i;
+            break;
+        }
+    }
+     std::cout << "Cycle start: " << cycleStart << "; Cycle end: " << cycleEnd << "; \n";
 
     // print rocks
     for (const auto& row: rocks) {
@@ -33,7 +102,7 @@ int main() {
     }
 
     // calculate and print structural stress
-    int totalLoad = 0;
+    size_t totalLoad = 0;
     size_t rows = rocks.size();
     for (int i = 0; i < rows; i++) {
         for (auto& rock: rocks[i]) {
